2e10cb1cb1ba3cf6fa7744835f3619abf685aa5d
Matteo <matteoosborne@gmail.com>
2019-07-03 09:52:34 -0700

    Update ForwardLoops.cpp

--- C + +/ForwardLoops.cpp
+++ C + +/ForwardLoops.cpp
@@ -1,1 +1,16 @@
+//$g++ hello2.cpp -o hello2.o
+include <iostream>
+using namespace std;
 
+int main(){
+	int i;
+	for (;;({
+	cout<<i<<" ";
+	if (i 1= o && i % 5 == 0)cout<<endl;
+	i = i +1;
+	J++
+	if(j> 1000000) break;
+	
+	}
+	return0
+}
